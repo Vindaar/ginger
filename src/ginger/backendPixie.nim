2444495c29e8b792496e935615cb57c250b35053
zetashift <rskaraya@gmail.com>
2021-05-17 12:37:25 +0200

    Implement more draw functions and refactor rotationing elements

--- src/ginger/backendPixie.nim
+++ src/ginger/backendPixie.nim
@@ -3,15 +3,17 @@ import options
 import pixie
 import types
 
-proc toVec2(point: Point): Vec2 =
+func toVec2(point: Point): Vec2 =
   # Helper to convert ginger's Points to vec2's for Pixie
   result = vec2(point.x, point.y)
 
+func rotationMatrix(angle: float, around: Point): Mat3 =
+  # Returns the transformation matrix enabling easy rotation around a point
+  result = translate(around.toVec2) * rotate((angle * PI / 180.0).float32) * translate(-around.toVec2)
+
 proc rotate(path: var Path, angle: float, around: Point) =
-  # We use the low-level Path API because it allows us rotation
-  path.transform(translate(around.toVec2))
-  path.transform(rotate((angle * PI / 180.0).float32))
-  path.transform(translate(-around.toVec2))
+  # We use the low-level Path API because it allows us more flexible rotation
+  path.transform(rotationMatrix(angle, around))
 
 proc drawLine*(img: BImage, start, stop: Point,
                style: Style,
@@ -19,31 +21,75 @@ proc drawLine*(img: BImage, start, stop: Point,
   var path: Path
   path.moveTo(start.toVec2)
   path.lineTo(stop.toVec2)
+
   if rotateAngle.isSome: 
     let (angle, around) = rotateAngle.get
     path.rotate(angle, around)
+
   img.pxImage.strokePath(path, style.color, style.lineWidth)
     
 proc drawPolyLine*(img: BImage, points: seq[Point],
                    style: Style,
                    rotateAngle: Option[(float, Point)] = none[(float, Point)]()) =
-  debugecho "WARNING: `drawPolyLine` of dummy backend is being called!"
+  # Draws lines sequentially to every point in `points`
+  var path: Path
+  let startPoint = points[0].toVec2
+  path.moveTo(startPoint)
+  
+  for idx in 1..points.high:
+    path.lineTo(points[idx].toVec2)
+
+  img.pxImage.strokePath(path, style.color, style.lineWidth)
+  img.pxImage.fillPath(path, style.fillColor)
 
 proc drawCircle*(img: BImage, center: Point, radius: float,
                  lineWidth: float,
                  strokeColor = color(0.0, 0.0, 0.0),
                  fillColor = color(0.0, 0.0, 0.0, 0.0),
                  rotateAngle: Option[(float, Point)] = none[(float, Point)]()) =
-  debugecho "WARNING: `drawCircle` of dummy backend is being called!"
+  var path: Path
+  path.circle(center.toVec2, radius)
+
+  if rotateAngle.isSome:
+    let (angle, around) = rotateAngle.get
+    path.rotate(angle, around)
+
+  img.pxImage.fillPath(path, fillColor)
+  img.pxImage.strokePath(path, strokeColor, lineWidth)
 
 proc getTextExtent*(text: string, font: types.Font): TextExtent =
-  debugecho "WARNING: `getTextExtent` of dummy backend is being called!"
+  debugecho "WARNING: `getTextExtent` of Pixie backend is being called and is unnessecary"
+
+func align(alignKind: TextAlignKind): tuple[HAlignMode, VAlignMode] =
+  # Return Pixie alignments given a Ginger text alignment
+  case alignKind:
+  of taLeft:
+    result = (haLeft, vaMiddle)
+  of taCenter:
+    result = (haCenter, vaMiddle)
+  of taRight:
+    result = (haRight, vaMiddle)
 
 proc drawText*(img: BImage, text: string, font: types.Font, at: Point,
                alignKind: TextAlignKind = taLeft,
                rotate: Option[float] = none[float](),
                rotateInView: Option[(float, Point)] = none[(float, Point)]()) =
-  debugecho "WARNING: `drawText` of dummy backend is being called!"
+  var pxFont = readFont(font.family) # TODO: family AFAIK does not point to a valid font file so this will fail
+  pxFont.size = font.size
+  # pxFont.paint.color = font.color
+  # TODO figure out font slant and Pixie
+
+  var transformMatrix: Mat3 = mat3()
+  # Rotate the text first and then align it
+  if rotateInView.isSome:
+    let (angle, around) = rotateInView.get
+    transformMatrix = transformMatrix * rotationMatrix(angle, around)
+  if rotate.isSome:
+    transformMatrix = transformMatrix * rotationMatrix(rotate.get, (at.x, at.y))
+
+  let (hAlign, vAlign) = align(alignKind)
+  # Render the text to the image using our transformation matrix and alignment
+  img.pxImage.fillText(pxFont, text, transformMatrix, hAlign, vAlign)
 
 proc drawRectangle*(img: BImage, left, bottom, width, height: float,
                     style: Style,
@@ -65,7 +111,7 @@ proc drawRaster*(img: var BImage, left, bottom, width, height: float,
                  drawCb: proc(): seq[uint32],
                  rotate: Option[float] = none[float](),
                  rotateInView: Option[(float, Point),] = none[(float, Point)]()) =
-  debugecho "WARNING: `drawRaster` of dummy backend is being called!"
+  debugecho "WARNING: `drawRaster` of Pixie backend is being called!"
 
 proc initBImage*(filename: string,
                  width, height: int,
