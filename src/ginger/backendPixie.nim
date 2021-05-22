bc4e1a982b6d61523c234c690b80a189a406c012
zetashift <rskaraya@gmail.com>
2021-05-22 17:13:22 +0200

    Add drawRaster

--- src/ginger/backendPixie.nim
+++ src/ginger/backendPixie.nim
@@ -14,7 +14,8 @@ proc saveState(img: BImage) =
   img.pxContext.resetTransform()
 
 proc rotate(img: BImage, angle: float, around: Point) =
-  # Rotate the given Context with `angle` around a Point
+  # Adds the matrix for rotating the given Context of `img` with `angle` around a Point
+  # to the Context
   img.pxContext.translate(around.toVec2)
   img.pxContext.rotate((angle * PI / 180.0).float32)
   img.pxContext.translate(-around.toVec2)
@@ -32,10 +33,12 @@ proc setStyle(img: BImage, style: Style) =
 proc drawLine*(img: BImage, start, stop: Point,
                style: Style,
                rotateAngle: Option[(float, Point)] = none[(float, Point)]()) =
-  if rotateAngle.isSome: 
+  # Draws a line from `start` to `stop`
+  # First we check if we need to rotate the line around a point
+  if rotateAngle.isSome:
     let (angle, around) = rotateAngle.get
     img.rotate(angle, around)
-  
+
   img.setStyle(style)
   img.pxContext.strokeSegment(segment(start.toVec2, stop.toVec2))
   img.saveState()
@@ -44,6 +47,10 @@ proc drawPolyLine*(img: BImage, points: seq[Point],
                    style: Style,
                    rotateAngle: Option[(float, Point)] = none[(float, Point)]()) =
   # Draws lines sequentially to every point in `points`
+  if rotateAngle.isSome:
+    let (angle, around) = rotateAngle.get
+    img.rotate(angle, around)
+
   var path: Path
   let startPoint = points[0].toVec2
   path.moveTo(startPoint)
@@ -53,7 +60,7 @@ proc drawPolyLine*(img: BImage, points: seq[Point],
 
   img.setStyle(style)
   img.pxContext.stroke(path)
-  # When drawing a line that closes a path, it will fill
+  # When drawing a line that closes a path, it will fill it
   img.pxContext.fill(path)
   img.saveState()
 
@@ -63,9 +70,8 @@ proc drawCircle*(img: BImage, center: Point, radius: float,
   if rotateAngle.isSome:
     let (angle, around) = rotateAngle.get
     img.rotate(angle, around)
-  
+
   img.setStyle(style)
-  
   img.pxContext.strokeCircle(center.toVec2, radius)
   img.pxContext.fillCircle(center.toVec2, radius)
   img.saveState()
@@ -90,13 +96,14 @@ proc drawText*(img: BImage, text: string, font: types.Font, at: Point,
   var pxFont = readFont(font.family) # TODO: family AFAIK does not point to a valid font file so this will fail
   pxFont.size = font.size
 
-  # Rotate the text first and then align it
+  # Check if we need to rotate around a specified Point on the canvas
   if rotateInView.isSome:
     let (angle, around) = rotateInView.get
     img.rotate(angle, around)
   if rotate.isSome:
+    # Here we only rotate with an angle around the origin if needed
     img.rotate(rotate.get, (at.x, at.y))
-  
+
   # TODO style text
   let (hAlign, vAlign) = getTextAligns(alignKind)
   img.pxContext.fillText(text, at.toVec2)
@@ -106,23 +113,64 @@ proc drawRectangle*(img: BImage, left, bottom, width, height: float,
                     style: Style,
                     rotate: Option[float] = none[float](),
                     rotateInView: Option[(float, Point),] = none[(float, Point)]()) =
+  # Check if we need to rotate around a specified Point on the canvas
   if rotateInView.isSome:
     let (angle, around) = rotateInView.get
     img.rotate(angle, around)
   if rotate.isSome:
+    # Here we only rotate with an angle around the origin if needed
     img.rotate(rotate.get, (left, bottom))
-  
+
   img.setStyle(style)
   let rect = rect(left, bottom, width, height)
   img.pxContext.strokeRect(rect)
   img.pxContext.fillRect(rect)
   img.saveState()
 
+func toColorRGBA(x: uint32): ColorRGBA =
+  # Converts uint32 to a Pixie compatible color for drawing a raster
+  let alpha = x shr 24 # no `and` necessary, since higher bits now all 0
+  let r = x shr 16 and 0xFF'u32
+  let g = x shr 8 and 0xFF'u32
+  let b = x and 0xFF'u32
+  result = ColorRGBA(r: r.uint8, g: g.uint8, b: b.uint8, a: alpha.uint8)
+
 proc drawRaster*(img: var BImage, left, bottom, width, height: float,
                  numX, numY: int,
                  drawCb: proc(): seq[uint32],
                  rotate: Option[float] = none[float](),
-                 rotateInView: Option[(float, Point),] = none[(float, Point)]()) = discard
+                 rotateInView: Option[(float, Point),] = none[(float, Point)]()) =
+  # Draws a raster for grid-like data, `drawCb` returns a 1D sequence
+  # representing a 2D grid. Based on it's index we compute the (x, y) coordinate
+
+  # Check if we need to rotate around a specified Point on the canvas
+  if rotateInView.isSome:
+    let (angle, around) = rotateInView.get
+    img.rotate(angle, around)
+  if rotate.isSome:
+    # Here we only rotate with an angle around the origin if needed
+    img.rotate(rotate.get, (left, bottom))
+
+  let
+    # Initialize our raster
+    rasterWidth = abs(width).int32
+    rasterHeight = abs(height).int32
+    raster = newImage(rasterWidth, rasterHeight)
+    # Dimensions of a single tile in pixels
+    tileWidth = width / numX.float
+    tileHeight = height / numY.float
+    toDraw = drawCb() # Get the sequence that holds the color information
+
+  for y in 0 ..< rasterHeight:
+    for x in 0 ..< rasterWidth:
+      # Get the element in `toDraw` and apply the color on the corresponding pixel on our raster
+      let tX = (x.float / tileWidth).floor.int
+      let tY = (y.float / tileHeight).floor.int
+      raster[x, y] = toDraw[tY * numX + tX].toColorRGBA
+
+  # Actually draw the raster on to the Context
+  img.pxContext.image.draw(raster, pos = vec2(left, bottom))
+  img.saveState()
 
 proc initBImage*(filename: string,
                  width, height: int,
