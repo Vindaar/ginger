93ccebbb35b1a15b89e4d7f7454ea441dece9ea2
Vindaar <basti90@gmail.com>
2021-07-22 19:10:49 +0200

    apply styling, allow text rotations

--- src/ginger/backendTikZ.nim
+++ src/ginger/backendTikZ.nim
@@ -10,37 +10,83 @@ Maybe we have to collect all colors in a table or seq and create a custom 'pream
 contains color definitons?
 ]#
 
-template toStr(p: Point): string {.dirty.} =
+proc toTikZCoord(img: BImage, p: Point, isLength: static bool = false): Point =
+  let ratio = img.height.float / img.width.float
+  when isLength:
+    result = (x: p.x / img.width.float, y: p.y / img.height.float)
+  else:
+    result = (x: p.x / img.width.float, y: (img.height.float - p.y) / img.height.float)
+  result.y = result.y * ratio
+
+func toStr(img: BImage, p: Point, isLength: static bool = false): string =
   block:
-    let pst = (x: p.x / img.width.float, y: (img.height.float - p.y) / img.height.float)
-    &"({pst.x}\\textwidth, {pst.y}\\textwidth)"
+    let pst = img.toTikZCoord(p, isLength = isLength)
+    var tmp = &"({pst.x}\\textwidth, {pst.y}\\textwidth)"
+    tmp
+
+func toStrDirect(p: Point, isLength: static bool = false): string =
+  block:
+    var tmp = &"({p.x}\\textwidth, {p.y}\\textwidth)"
+    tmp
+
+proc toStr(alignKind: TextAlignKind): string =
+  ## convert the text align kind to the correct tikz notation
+  case alignKind
+  of taLeft: result = "right"
+  of taCenter: result = "" # default
+  of taRight: result = "left"
+
+proc nodeProperties(img: BImage, at: Point, alignKind: TextAlignKind, rotate: Option[float]): string =
+  result = "["
+  result.add alignKind.toStr
+  if rotate.isSome:
+    #result.add ", rotate around = {" & $(-rotate.get) & ":" & at.toStr & "}" # rotation opposite of cairo
+    result.add ", rotate = " & $(-rotate.get) # rotation opposite of cairo
+  result.add "]"
+  echo result
 
 template latexAdd(body: untyped): untyped {.dirty.} =
   let toAdd = latex:
     body
   img.data.add toAdd
 
+proc defColor(name: string, c: Color): string =
+  let color = &"{c.r * 256.0}, {c.g * 256.0}, {c.b * 256.0}"
+  result = latex:
+    \definecolor{`name`}{RGB}{`color`}
+
+proc colorStr(style: Style): string =
+  result.add defColor("drawColor", style.color)
+  result.add defColor("fillColor", style.fillColor)
+
+proc lineStyle(style: Style, drawColor = "drawColor", fillColor = "fillColor"): string =
+  #let color = "{" & &"{style.color.r * 256.0}, {style.color.g * 256.0}, {style.color.b * 256.0}" & "}"
+  let fillOp = &"fill opacity = {style.fillColor.a}"
+  let colorOp = &"draw opacity = {style.color.a}"
+  result = &"[color = {drawColor}, fill = {fillColor}, {colorOp}, {fillOp}, line width = {style.lineWidth}pt]"
+
 proc drawLine*(img: var BImage, start, stop: Point,
                style: Style,
                rotateAngle: Option[(float, Point)] = none[(float, Point)]()) =
-  let p0 = start.toStr
-  let p1 = stop.toStr
-  let colors = &"{style.color.r}, {style.color.g}, {style.color.b}" # , {style.color.a}
-  let color = latex:
-    \definecolor{foocolor}{RGB}{`colors`}
-  let lineWidth = &"[line width = {style.lineWidth}pt, fill = foocolor]"
-  #,fill={rgb:red,4;green,2;yellow,1}
+  let p0 = img.toStr(start)
+  let p1 = img.toStr(stop)
+  let color = style.colorStr
+  let lineSt = style.lineStyle
   latexAdd:
     `color`
-    \draw " " `lineWidth` " " `p0` " " -- " " `p1` ";"
+    \draw " " `lineSt` " " `p0` " " -- " " `p1` ";"
 
 proc drawPolyLine*(img: var BImage, points: seq[Point],
                    style: Style,
                    rotateAngle: Option[(float, Point)] = none[(float, Point)]()) =
+  let lineSt = style.lineStyle
+  let color = style.colorStr
+  echo lineSt
   latexAdd:
-    \draw
+    `color`
+    \draw " " `lineSt`
   for i, p in points:
-    let pStr = p.toStr
+    let pStr = img.toStr(p)
     if i == points.high:
       latexAdd:
         `pStr`
@@ -54,18 +100,26 @@ proc drawCircle*(img: var BImage, center: Point, radius: float,
                  strokeColor = color(0.0, 0.0, 0.0),
                  fillColor = color(0.0, 0.0, 0.0, 0.0),
                  rotateAngle: Option[(float, Point)] = none[(float, Point)]()) =
-  let p = center.toStr
+  let p = img.toStr(center)
+  let radius = $(radius / img.width.float) & "\\textwidth"# / img.width.float
+  let style = Style(lineWidth: lineWidth,
+                    color: strokeColor,
+                    fillColor: fillColor)
+  let color = style.colorStr
+  let lineSt = style.lineStyle
   latexAdd:
-    \draw " " `p` " " circle " " [radius = `radius`] ";"
+    `color`
+    \draw " " `lineSt` " " `p` " " circle " " [radius = `radius`] ";"
 
 proc getTextExtent*(text: string, font: Font): TextExtent =
   ## XXX: HACK
-  const pt = 20.0
+  let ptY = font.size
+  let ptX = ptY
   result = TextExtent(
     x_bearing: 0.0,
     y_bearing: 0.0,
-    width: pt * text.len.float,
-    height: pt)
+    width: ptX * text.len.float,
+    height: ptY)
   result.x_advance = result.width
   result.y_advance = result.height
 
@@ -73,18 +127,71 @@ proc drawText*(img: var BImage, text: string, font: Font, at: Point,
                alignKind: TextAlignKind = taLeft,
                rotate: Option[float] = none[float](),
                rotateInView: Option[(float, Point)] = none[(float, Point)]()) =
-  let atStr = at.toStr
+  ## TODO: fix this coord mess
+  var
+    x = at.x
+    y = at.y
+
+  let extents = getTextExtent("M", font)
+  # potentially rotate around specific point (location depends on where we align)
+  case alignKind
+  of taLeft:
+    x = at.x - (extents.width / 2.0 + extents.x_bearing)
+  of taRight:
+    x = at.x + (extents.width / 2.0 + extents.x_bearing)
+  of taCenter:
+    #x = at.x - (extents.width / 2.0 + extents.x_bearing)
+    #y = at.y + (extents.height / 2.0 + extents.y_bearing)
+    discard
+  else: discard
+
+  let at = (x: x, y: y)
+  let atStr = img.toStr(at)
+  let alignStr = img.nodeProperties(at, alignKind, rotate)
+  echo "ext at ", atStr, " with ", alignStr, " of ", text, " from ", at
+  let ts = $(font.size)
+  let tst = $((font.size) * 1.2)
+  var text = latex:
+    \fontsize{`ts`}{`tst`}\selectfont " " `text`
+  #if rotate.isSome:
+  #  let rt = $(-rotate.get)
+  #  text = latex:
+  #    \rotatebox{`rt`}{`text`}
   latexAdd:
-    \node " at " `atStr` {`text`} ";"
+    \node " " `alignStr` " at " `atStr` {`text`} ";"
 
 proc drawRectangle*(img: var BImage, left, bottom, width, height: float,
                     style: Style,
                     rotate: Option[float] = none[float](),
                     rotateInView: Option[(float, Point),] = none[(float, Point)]()) =
-  let atStr = (x: left, y: bottom).toStr
-  let sizeStr = (x: width, y: height).toStr
-  latexAdd:
-    \draw " " `atStr` " " rectangle " " `sizeStr` ";"
+  let atPt = img.toTikZCoord((x: left, y: bottom))
+  let sizePt = img.toTikZCoord((x: left + width, y: bottom + height), isLength = false)
+  if style.gradient.isSome:
+    let gradient = style.gradient.get
+    let heightRel = height / img.width.float
+    let sliceHeight = heightRel / (gradient.colors.len.float - 1)
+    var curBottom = atPt.y - heightRel
+    for i, c in gradient.colors:
+      let n = "color" & $i
+      let atStr = (x: atPt.x, y: curBottom).toStrDirect # left bottom coords
+      let sizeStr = (
+        x: sizePt.x,
+        y: curBottom + sliceHeight + 1e-3               # add some ε for overlap
+      ).toStrDirect                                     # end coords
+      let curColor = defColor(n, c)
+      let curLineStyle = style.lineStyle(fillColor = n)
+      latexAdd:
+        `curColor`
+        \draw " " `curLineStyle` " " `atStr` " " rectangle " " `sizeStr` ";"
+      curBottom += sliceHeight
+  else:
+    let color = style.colorStr
+    let lineSt = style.lineStyle
+    let sizeStr = sizePt.toStrDirect
+    let atStr = atPt.toStrDirect
+    latexAdd:
+      `color`
+      \draw " " `lineSt` " " `atStr` " " rectangle " " `sizeStr` ";"
 
 proc drawRaster*(img: var BImage, left, bottom, width, height: float,
                  numX, numY: int,
